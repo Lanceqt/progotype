efc0e14c99a3e6c82848fae16d50c1bfe7678efd
Lanceqt <andreas.frederiksen@proton.me>
2023-09-02 23:28:48 +0200

    progotype idea

--- cmd/progotype/main.go
+++ cmd/progotype/main.go
@@ -9,27 +9,33 @@ import (
 )
 
 func main() {
+	// IMPORTANT:
+	// Please ensure that the path is correctly specified relative to this file.
 	rootPath := "../../server"
+	defaultFolder := "index" // Change the folder name here
+	//src := "src"
 	mux := http.NewServeMux()
+
 	err := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if info.IsDir() {
-			return nil
+		if info.IsDir() && path != rootPath {
+			relPath, err := filepath.Rel(rootPath, path)
+			if err != nil {
+				return err
+			}
+			mux.Handle("/"+relPath+"/", http.StripPrefix("/"+relPath+"/", http.FileServer(http.Dir(path))))
 		}
-		relPath, err := filepath.Rel(rootPath, path)
-		if err != nil {
-			return err
-		}
-		mux.HandleFunc("/"+relPath, func(w http.ResponseWriter, r *http.Request) {
-			http.ServeFile(w, r, path)
-		})
 		return nil
 	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	mux.Handle("/", http.FileServer(http.Dir(filepath.Join(rootPath, defaultFolder))))
+
 	fmt.Println("Server started on http://localhost:8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", mux))
 }
