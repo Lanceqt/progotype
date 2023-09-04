8ef87d286ef188ba5efb53fcc747f9ea447b1cfb
Lanceqt <andreas.frederiksen@proton.me>
2023-09-04 02:15:55 +0200

    File routing is mostly working

--- cmd/progotype/main.go
+++ cmd/progotype/main.go
@@ -1,41 +1,98 @@
+// don't change anything in this file if you don't know what you're doing
+// It will break the server if you change anything in this file
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
+	"mime"
 	"net/http"
 	"os"
 	"path/filepath"
 )
 
 func main() {
-	// IMPORTANT:
-	// Please ensure that the path is correctly specified relative to this file.
+	// CONFIG:
+	// rootPath is your web server folder
+	// defaultFolder is the root index server
+	// src is the folder where your assets are
 	rootPath := "../../server"
-	defaultFolder := "index" // Change the folder name here
-	//src := "src"
-	mux := http.NewServeMux()
+	defaultFolder := "index"
+	src := "src"
 
-	err := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
+	// Parse the base template
+	baseTemplate, err := template.ParseFiles(filepath.Join(rootPath, "layout.html"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/"+src+"/", func(w http.ResponseWriter, r *http.Request) {
+		// Get the requested file path
+		filePath := filepath.Join(rootPath, r.URL.Path)
+
+		// Check if the requested file exists
+		fileInfo, err := os.Stat(filePath)
+		if err != nil || fileInfo.IsDir() {
+			// File does not exist or is a directory, return a 404 error
+			http.NotFound(w, r)
+			return
+		}
+
+		// Serve the requested file
+		contentType := mime.TypeByExtension(filepath.Ext(filePath))
+		if contentType != "" {
+			// Set the content type for known file types
+			w.Header().Set("Content-Type", contentType)
+		}
+		http.ServeFile(w, r, filePath)
+	})
+
+	err = filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if info.IsDir() && path != rootPath {
+		if info.IsDir() && path != rootPath && filepath.Base(path) != src {
 			relPath, err := filepath.Rel(rootPath, path)
 			if err != nil {
 				return err
 			}
-			mux.Handle("/"+relPath+"/", http.StripPrefix("/"+relPath+"/", http.FileServer(http.Dir(path))))
+
+			// Parse the template for this folder
+			templatePath := filepath.Join(path, filepath.Base(path)+".html")
+			tmpl, err := template.Must(baseTemplate.Clone()).ParseFiles(templatePath)
+			if err != nil {
+				return err
+			}
+
+			// Serve the template
+			mux.HandleFunc("/"+relPath+"/", func(w http.ResponseWriter, r *http.Request) {
+				err := tmpl.Execute(w, nil)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+			})
 		}
-		return nil
+		return filepath.SkipDir
 	})
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(filepath.Join(rootPath, defaultFolder))))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.Must(baseTemplate.Clone()).ParseFiles(filepath.Join(rootPath, defaultFolder+"/"+defaultFolder+".html"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	fmt.Println("Server started on http://localhost:8080")
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	log.Println("Server started on http://localhost:8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", mux))
 }
